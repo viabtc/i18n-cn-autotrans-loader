b411175083168cad5300b5e6c64278a21cac4868
dengshen <dengshendengshen@gmail.com>
2018-05-31 16:42:32 +0800

    better regex for chinese in script

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cnAttrReg = new RegExp(
   "igm"
   ),
   cnCodeReg = new RegExp(
-    /(?:[^=])(?:['"])([\u4e00-\u9fa5\u3002\uff1b\uff0c\uff1a\u2018\u2019\u201c\u201d\uff08\uff09\u3001\uff1f\uff01\ufe15\u300a\u300b]+(?:\s*)[^\'`"<]*)(?:[\'"/])/,
+    /(?:[^=])(?:['"])([^"'<>{}\.]*?[\u4e00-\u9fa5\u3002\uff1b\uff0c\uff1a\u2018\u2019\u201c\u201d\uff08\uff09\u3001\uff1f\uff01\ufe15\u300a\u300b]+(?:\s*)[^\'`"<]*)(?:[\'"/])/,
     "igm"
   ),
   cnTemplateReg = new RegExp(
